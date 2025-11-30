d87e83c5c3035bd7400cd8d390b33804d26dfc63
Waleola <waleolaogunjimi28@gmail.com>
2025-11-30 22:15:24 +0100

    Add Book and Library classes for library management system

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,52 @@
+class Book:
+    """A class representing a single book in the library."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Return True if the book is not checked out."""
+        return not self._is_checked_out
+
+
+class Library:
+    """A class representing a library that manages multiple books."""
+
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a book instance to the library collection."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if it is available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False
+
+    def list_available_books(self):
+        """Print all available books in 'Title by Author' format."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
